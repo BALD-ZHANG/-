31832c9764976e4cbbf969628eae091ab1136761
BALD-ZHANG <34709628+BALD-ZHANG@users.noreply.github.com>
2017-12-20 18:32:15 +0800

    Create chaxun.js

---
+++ chaxun.js
@@ -0,0 +1,154 @@
+var myCharts = require("../../../utils/wxcharts.js")//引入一个绘图的插件
+var lineChart_noise = null
+var lineChart_light = null
+var lineChart_tempe = null
+var app = getApp()
+
+Page({
+  data: {
+  },
+  onPullDownRefresh: function () {
+    console.log('onPullDownRefresh', new Date())
+  },
+
+
+  //把拿到的数据转换成绘图插件需要的输入格式
+  convert: function () {
+    var categories = [];
+    var noise = [];
+    var light = [];
+    var tempe = [];
+
+    var length = app.globalData.light.datapoints.length
+    for (var i = 0; i < length; i++) {
+      categories.push(app.globalData.noise.datapoints[i].at.slice(11, 19));
+      noise.push(app.globalData.noise.datapoints[i].value);
+      light.push(app.globalData.light.datapoints[i].value);
+      tempe.push(app.globalData.temperature.datapoints[i].value);
+    }
+    return {
+      categories: categories,
+      noise: noise,
+      light: light,
+      tempe: tempe
+    }
+  },
+
+  onLoad: function () {
+    var wheatherData = this.convert();
+
+    //得到屏幕宽度
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var wheatherData = this.convert();
+
+    //新建光照强度图表
+    lineChart_noise = new myCharts({
+      canvasId: 'noise',
+      type: 'line',
+      categories: wheatherData.categories,
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: 'noise',
+        data: wheatherData.light,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: 'noise (db)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 190
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+
+
+    //新建噪音图表
+    lineChart_light = new myCharts({
+      canvasId: 'light',
+      type: 'line',
+      categories: wheatherData.categories,
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: 'light',
+        data: wheatherData.noise,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: 'light (lux)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 55
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+
+
+    //新建温度图表
+    lineChart_tempe = new myCharts({
+      canvasId: 'tempe',
+      type: 'line',
+      categories: wheatherData.categories,
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: 'temperature',
+        data: wheatherData.tempe,
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: 'temperature (摄氏度)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 24
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+  },
+
+
+})
